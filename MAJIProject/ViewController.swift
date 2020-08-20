07eb838e06ad2aa8c303b2ade7ba9752613b0e15
Mac <908693181@qq.com>
2020-08-20 17:39:28 +0800

    Initial Commit

---
+++ MAJIProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MAJIProject
+//
+//  Created by Mac on 2020/8/20.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
